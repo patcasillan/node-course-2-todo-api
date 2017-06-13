d8c41bf08631fbea17829007f2bde36278598f67
Patrick Casillan <pjcasillan@Patricks-MacBook-Pro.local>
2017-06-13 08:45:49 +0800

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to the MongoDB server.')
+    }
+    console.log('Connected to MongoDB server.')
+
+//FindOneAndUpdate
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('593e13496de3a8bf428eec45')
+    // }, {
+    //     $set: {
+    //      completed: true   
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+//FindOneandUpdate and Increment
+    db.collection('Users').findOneAndUpdate({
+        name: 'Pat'
+    }, {
+        $set: {
+            name: 'Patrick'
+        }, 
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    db.close();
+}); 
